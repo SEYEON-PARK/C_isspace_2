2cbad8800333782978829f53d694dcc314729d64
SEYEON_PARK <fourleaf123@naver.com>
2021-03-09 00:36:04 +0900

    C언어 <ctype.h>의 isspace(공백문자인지 아닌지 확인해준다.) 함수 연습!(2)

---
+++ C_isspace_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char a = ' ';
+
+	if (isspace != 0)
+	{
+		printf("공백 문자입니다.\n");
+	}
+	else
+	{
+		printf("공백 문자가 아닙니다.\n");
+	}
+
+	return 0;
+}
